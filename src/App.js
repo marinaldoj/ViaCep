efc9f1acce57874f41acc55e76382e3fd2679978
Marinaldo Junior <carvalho.junior@soterotech.com.br>
2021-03-22 21:05:12 -0300

    Meu Projeto via cep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './styles/global.css'
 
 function App() {
+  const [logradouro, setLogradouro] = useState('');
+  const [uf, setUf] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [localidade, setLocalidade] = useState('');
+  const [cep, setCep] = useState('');
+
+  const getCep = async () => {
+    if(cep.length >=8){
+      const data = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(json => {return(json)})
+  
+      setUf(data.uf);
+      setBairro(data.bairro);
+      setLocalidade(data.localidade);
+      setLogradouro(data.logradouro);    
+    }  
+  }
+
+  const limparEstados = () => {
+    setLogradouro('')
+    setUf('')
+    setBairro('')
+    setLocalidade('')
+    setCep('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="Container">
+        <div className="Body-Container">
+          <h1>
+            Via Cep
+          </h1>
+          <div className="Input-Container">
+            <input placeholder="Digite seu cep:" maxLength={8} value={cep} onChange={(e) => {setCep(e.target.value)}}></input>
+          </div>
+          <div className="Values-Container">
+            <input placeholder="Logradouro" value={logradouro}></input>
+            <input placeholder="Localidade" value={localidade}></input>
+            <input placeholder="Bairro" value={bairro}></input>
+            <input placeholder="UF" value={uf}></input>
+          </div>
+          <div className="div-buttons">
+            <button onClick={() => getCep()}>Buscar</button>
+            <button onClick={() => limparEstados()}>Limpar</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
